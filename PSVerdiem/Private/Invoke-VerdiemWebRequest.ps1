652803bf52564cea2fd947b04bf37d731cd4b15c
Sean R. Williams <hmmwhatsthisdo@gmail.com>
2017-07-02 00:56:07 -0700

    Added basic webrequest functionality for communicating with Verdiem

---
+++ PSVerdiem/Private/Invoke-VerdiemWebRequest.ps1
@@ -0,0 +1,49 @@
+﻿Function Invoke-VerdiemWebRequest {
+[CmdletBinding()]
+Param(
+    [Parameter(
+        Mandatory=$true,
+        Position=0
+    )]
+    [ValidateNotNullOrEmpty()]
+    [String]$Server,
+
+    [Switch]$Secure,
+
+    [ValidateNotNullOrEmpty()]
+    [System.Management.Automation.CredentialAttribute()]
+    [pscredential]$Credential, # = (Get-VerdiemLastCredential -Server $Server),
+
+    [String]$ServicePath = "Admin",
+
+    [String]$ServiceName = "AdminService.svc",
+
+    [String]$MethodName,
+
+
+    [Object]$Body,
+
+    [Switch]$Raw
+)
+
+    # Set-VerdiemLastCredential -Server $Server -Credential $Credential -Secure $Secure
+
+    $Endpoint = ((&{If ($secure) {"https"} Else {"http"}}) + "://" + $Server)
+
+    $Params = @{
+        Uri = "$Endpoint/$ServicePath/$ServiceName/$MethodName"
+        Method = "Post"
+        Credential = $Credential
+        Body = ($Body | ConvertTo-Json -Depth 100 -Compress)
+        ContentType = "application/json"
+    }
+
+    If ($Raw) {
+        return Invoke-WebRequest @params | % Content
+    } Else {
+        return Invoke-RestMethod @params
+    }
+
+    
+    
+}
