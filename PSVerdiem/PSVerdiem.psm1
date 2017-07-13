d85c53a3a3e2dbdd7b31bcde4742a5416d8aa084
Sean R. Williams <hmmwhatsthisdo@gmail.com>
2017-07-13 00:51:14 -0700

    Auto-register all types marked as serializeable from any assembly we've loaded with the JSONTypeResolver

--- PSVerdiem/PSVerdiem.psm1
+++ PSVerdiem/PSVerdiem.psm1
@@ -20,10 +20,11 @@ foreach ($Type in @("Public","Private")) {
 }
 
 Write-Verbose "Importing Libraries..."
+$Assemblies = @{}
 foreach ($Library in (Get-Childitem "$PSScriptRoot\Library\*.dll")) {
 	Write-Verbose "Importing $($Library.Basename)..."
 	try {
-		Add-Type -Path $Library.Fullname
+		$Assemblies[$Library.BaseName] = Add-Type -Path $Library.Fullname -PassThru
 	} catch {
 		Write-Error "Failed to import DLL $($Library.BaseName): $_"
 	}
@@ -31,6 +32,9 @@ foreach ($Library in (Get-Childitem "$PSScriptRoot\Library\*.dll")) {
 
 # Create a TypeResolver for Verdiem JSON types
 $Script:JSONTypeResolver = [PSVerdiem.StatefulTypeResolver]::new()
+
+# Register any types in assemblies we've imported that are tagged as serializable
+$Assemblies.GetEnumerator() | % Value | ? IsSerializable | % {$Script:JSONTypeResolver.RegisterType($_.Name, $_)}
 
 # Export our public functions, now that they exist
 $Scripts.Public | ForEach-Object BaseName | Export-ModuleMember 
